aa6797e7bdc07493f418f9ea8878ac5d6911b944
Caden Christesen <baptistchristesen@gmail.com>
2023-10-13 09:26:06 -0500

    Initial Commit

---
+++ Microphone input tester/Microphone_input_testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Microphone_input_testerApp.swift
+//  Microphone input tester
+//
+//  Created by Caden Christesen on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Microphone_input_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
